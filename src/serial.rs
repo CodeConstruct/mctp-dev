625d8dd4b0cf97460861a8ea9d279a2e4598cbc4
Jeremy Kerr <jk@codeconstruct.com.au>
2025-03-28 15:16:56 +0800

    LICENSE: Apply GPLv2 license
    Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

--- src/serial.rs
+++ src/serial.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0
+//
 use anyhow::{Context, Result};
 use embedded_io_adapters::futures_03::FromFutures;
 use smol::Async;
--- src/usbredir.rs
+++ src/usbredir.rs
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 
 use anyhow::{Context, Result};
 use mctp_estack::{Stack, SendOutput, usb::MctpUsbHandler, usb::MctpUsbXfer};
