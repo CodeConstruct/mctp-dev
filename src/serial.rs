212342c18932be291bfdc9aaf3214dfe63b46718
Jeremy Kerr <jk@codeconstruct.com.au>
2025-06-05 13:21:56 +0800

    Relicence as GPL-3.0
    ... as it is compatible with our Apache-2.0 dependencies. This means we
    have compatibility with all dependencies:
    
        $ cargo license | cut -f1 -d:
        (MIT OR Apache-2.0) AND Unicode-3.0 (1)
        Apache-2.0 OR Apache-2.0 WITH LLVM-exception OR MIT (4)
        Apache-2.0 OR LGPL-2.1-or-later OR MIT (1)
        Apache-2.0 OR MIT (99)
        Apache-2.0 WITH LLVM-exception (1)
        BSD-3-Clause (3)
        GPL-3.0 (1)
        MIT (5)
        MIT OR Unlicense (4)
    
    All contributions to this point have been from Code Construct.
    
    Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

--- src/serial.rs
+++ src/serial.rs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-3.0
 //
 use anyhow::{Context, Result};
 use embedded_io_adapters::futures_03::FromFutures;
--- src/usbredir.rs
+++ src/usbredir.rs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-3.0
 
 use anyhow::{Context, Result};
 use futures::{future, select, FutureExt};
