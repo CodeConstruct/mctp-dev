2eb276cc1cdb883f42d52a4bcdfe64e63c24a332
Jeremy Kerr <jk@codeconstruct.com.au>
2025-07-30 15:12:30 +0800

    ci: cover feature sets, run clippy
    Since we have a feature, ensure we build all configs. Run clippy on
    each.
    
    Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

--- ci/runtests.sh
+++ ci/runtests.sh
@@ -6,6 +6,26 @@ export RUSTDOCFLAGS='-D warnings'
 export RUSTFLAGS="-D warnings"
 
 cargo fmt -- --check
+
+# base checks
+cargo check --locked
 cargo build --release
-cargo test
 cargo doc
+cargo test
+
+# features
+sets=(
+    ""
+    "nvme-mi"
+)
+
+for features in "${sets[@]}"
+do
+    feature_args='--no-default-features'
+    if [ ${#features} -gt 0 ]
+    then
+        feature_args="$feature_args --features $features"
+    fi
+    cargo build $feature_args
+    cargo clippy $feature_args
+done
